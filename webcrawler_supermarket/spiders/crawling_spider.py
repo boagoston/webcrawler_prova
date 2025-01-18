3d5c2a70745127398e70894a408f1d64af51e9b6
Bruno Agoston <boagoston@gmail.com>
2025-01-18 18:49:15 -0300

    logica para extrair categorias foi alterada e simplificada

--- webcrawler_supermarket/spiders/crawling_spider.py
+++ webcrawler_supermarket/spiders/crawling_spider.py
@@ -1,5 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+import logging
 
 class CrawlingSpider(CrawlSpider):
     name = "crawling_compra_agora"
@@ -7,13 +8,24 @@ class CrawlingSpider(CrawlSpider):
     start_urls = ["https://www.compra-agora.com/"]
 
     rules = (
-        # LinkExtractor para categorias principais, o regex está buscando URLs que seguem este formato: /loja/{categoria}/{id}
-        Rule(LinkExtractor(allow=r'/loja/([^/]+)/\d+$'), callback='parse_categoria', follow=True),
+        # LinkExtractor para categorias principais. O regex está buscando URLs que seguem este formato: /loja/{categoria}/{id}
+        # É possível fazer o recorte das URLs por regex utilizando o LinkExtractor, mas é importante ter cuidado para não capturar URLs indesejadas
+        # alem disso, legibilidade do codigo é comprometida
+
+        #Rule(LinkExtractor(allow=r'/loja/([^/]+)/\d+$'), callback='parse_categoria', follow=True),
     )
+
 
     def parse_categoria(self, response):
 
-        # Exibindo o nome da categoria e a URL
-        yield {
-            'url': response.url
-        }
+        hover_menu_items = response.css("ul.hover-menu > li.lista-menu-itens")
+        
+        for item in hover_menu_items:
+            url = item.css("a::attr(href)").get(default="").strip()
+            logging.info(f"URL extraída: {url}")
+
+            if url:
+                logging.info(f"URL: {url}")
+                yield {
+                    'url': url,
+                }
